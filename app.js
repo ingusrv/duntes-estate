1f67d41e3b7d6406cbf826460215d211c22e7458
ingusrv <ingus.valheims@gmail.com>
2023-04-25 12:40:13 +0300

    salabota navigācija

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/konsultacijas", (req, res) => {
-    res.redirect("https://siguldaspv.edu.lv/wp-content/uploads/2023/02/Kons_graf_2_sem_22_23.pdf");
+    res.status(200).sendFile(path.join(__dirname, "public/konsultacijas/konsultacijas.html"));
 });
 
 // startējam serveri
