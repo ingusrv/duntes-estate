668d2986339fda3687725755ed36d3572b6aed79
ingusrv <ingus.valheims@gmail.com>
2023-04-26 07:30:45 +0300

    pievienota lasīšana 7.-9. klasēm

--- app.js
+++ app.js
@@ -69,6 +69,21 @@ const offsets = [
         "6.b": "AN3:AP14",
         "6.c": "AR3:AT14",
         "6.s": "AV3:AX14"
+    },
+    {
+        "worksheet": "7.-9._6_02_2023",
+        "irVirzieni": false,
+        "klase": ["D2", "H2", "L2", "P2", "T2", "X2", "AB2", "AF2", "AJ2", "AN2"],
+        "7.a": "D3:F14",
+        "7.b": "H3:J14",
+        "7.c": "L3:N14",
+        "8.a": "P3:R14",
+        "8.b": "T3:V14",
+        "8.c": "X3:Z14",
+        "8.d": "AB3:AD14",
+        "9.a": "AF3:AH14",
+        "9.b": "AJ3:AL14",
+        "9.c": "AN3:AP14",
     }
 ];
 
@@ -119,7 +134,7 @@ offsets[1].klase.forEach(klase_offset => {
         let stundu_offset = offsets[1][klase];
         let start_offset = Number.parseInt(stundu_offset.match(/\d/)[0]) + 10 * day + 1;
         let end_offset = Number.parseInt(stundu_offset.match(/\d\d/)[0]) + 10 * day + 1;
-        // RODO: izņemt šo
+        // TODO: izņemt šo
         if (day === 0) {
             start_offset -= 1;
             end_offset -= 1;
@@ -178,7 +193,81 @@ offsets[1].klase.forEach(klase_offset => {
     }
 });
 
-console.log("stundu saraksts", saraksts["4.a"][0]);
+// 7. līdz 9. klase
+offsets[2].klase.forEach(klase_offset => {
+    worksheet = workbook.Sheets[offsets[2].worksheet];
+    const klase = worksheet[klase_offset].v.match(/\d.\w/)[0];
+    console.log("klase: ", klase);
+    saraksts[klase] = [];
+
+    for (let day = 0; day < days; day++) {
+        let stundu_offset = offsets[2][klase];
+        let start_offset = Number.parseInt(stundu_offset.match(/\d/)[0]) + 10 * day + 1;
+        let end_offset = Number.parseInt(stundu_offset.match(/\d\d/)[0]) + 10 * day + 1;
+        // TODO: izņemt šo
+        if (day === 0) {
+            start_offset -= 1;
+            end_offset -= 1;
+        }
+        if (day === 1 || day === 2) {
+            end_offset += 1;
+        }
+        if (day === 2) {
+            start_offset += 2;
+            end_offset += 2;
+        }
+        if (day === 3) {
+            start_offset += 4;
+            end_offset += 4;
+        }
+        if (day === 4) {
+            start_offset += 5;
+            end_offset += 5;
+        }
+        stundu_offset = stundu_offset.replace(/\d\d/, end_offset);
+        stundu_offset = stundu_offset.replace(/\d/, start_offset);
+        console.log("start", start_offset, "end", end_offset);
+        console.log("offset: ", stundu_offset);
+        worksheet["!ref"] = stundu_offset;
+        const stundas = xlsx.utils.sheet_to_json(worksheet, { defval: "" });
+        // console.log(stundas);
+        const diena = { diena: day, stundas: [] }
+        let n = 1;
+        stundas.forEach((stunda, i) => {
+            if (i === 1 || i === 2) {
+                return;
+            }
+
+            const s = {
+                numurs: n,
+                divi: false,
+                nosaukums: undefined,
+                kabinets: undefined,
+            };
+            const v = Object.values(stunda);
+            if (v.length === 3) {
+                s.divi = true;
+                s.nosaukums = v[0];
+                s.nosaukums2 = v[1];
+                const kabineti = v[2].split("/");
+                s.kabinets = kabineti[0];
+                s.kabinets2 = kabineti[1];
+                diena.stundas.push(s);
+                n += 1;
+                return;
+            }
+
+            s.nosaukums = v[0];
+            s.kabinets = v[1];
+
+            diena.stundas.push(s);
+            n += 1;
+        });
+        saraksts[klase].push(diena);
+    }
+});
+
+console.log("stundu saraksts", saraksts["7.a"][0]);
 
 // izmantojam middleware
 app.use(express.json());
